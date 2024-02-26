437db13bde1b780b9d23a48ff0e96df08676c072
ev-ev <ev@ev.me>
2024-02-26 18:17:28 +0800

    Added some more tests

--- test/test.c
+++ test/test.c
@@ -54,9 +54,9 @@ int testMakeNewDocument(HWND hwnd, HANDLE handle, struct StateInfo* pState) {
     return assert;
 }
 
+const char TEST_STRING_BASIC_TEXT_ENTRY[] = "\n\nText for testing out if entring text without special symbols works.\n";
 int testBasicTextEntry(HWND hwnd, HANDLE handle, struct StateInfo* pState) {
     struct StateInfo State = {0};
-    char TEST_STRING[] = "\n\nText for testing out if entring text without special symbols works.\n";
     int pass = 1;
     
     if (!ReadProcessMemory(handle, pState, &State, sizeof(State), 0)) return -1;
@@ -64,14 +64,14 @@ int testBasicTextEntry(HWND hwnd, HANDLE handle, struct StateInfo* pState) {
     char* old_text_entry = 0;
     int old_text_len = LLCHAR_to_pchar(handle, State.head, &old_text_entry);
     
-    for (int i = 0; i < sizeof(TEST_STRING) - 1; i++) {
-        SendMessage(hwnd, WM_CHAR, TEST_STRING[i], 0);
+    for (int i = 0; i < sizeof(TEST_STRING_BASIC_TEXT_ENTRY) - 1; i++) {
+        SendMessage(hwnd, WM_CHAR, TEST_STRING_BASIC_TEXT_ENTRY[i], 0);
     }
     
     char* new_text_entry = 0;
     int new_text_len = LLCHAR_to_pchar(handle, State.head, &new_text_entry);
     
-    if (strncmp(TEST_STRING, new_text_entry + old_text_len, new_text_len - old_text_len) == 0) {
+    if (strncmp(TEST_STRING_BASIC_TEXT_ENTRY, new_text_entry + old_text_len, new_text_len - old_text_len) == 0) {
         pass = 0; //Don't want to set directly due to return value of strncmp
     }
     
@@ -81,10 +81,10 @@ int testBasicTextEntry(HWND hwnd, HANDLE handle, struct StateInfo* pState) {
     return pass;
 }
 
+const char TEST_STRING_SPECIAL_KEY_TEXT_ENTRY[]  = "\n\nThis etxt\b\b\b\btxet\b\b\bext contains special symbolz \b\bs to check if\n\tthey work\rthis was a CR only line\n";
+const char TEST_STRING_REAL_SPECIAL_KEY_TEXT_ENTRY[] = "\n\nThis text contains special symbols to check if\n    they work\nthis was a CR only line\n";
 int testSpecialKeyTextEntry(HWND hwnd, HANDLE handle, struct StateInfo* pState) {
     struct StateInfo State = {0};
-    char TEST_STRING[] = "\n\nThis etxt\b\b\b\btxet\b\b\bext contains special symbolz \b\bs to check if\n\tthey work\rthis was a CR only line\n";
-    char REAL_TEST_STRING[] = "\n\nThis text contains special symbols to check if\n    they work\nthis was a CR only line\n";
     int pass = 1;
     
     if (!ReadProcessMemory(handle, pState, &State, sizeof(State), 0)) return -1;
@@ -92,21 +92,80 @@ int testSpecialKeyTextEntry(HWND hwnd, HANDLE handle, struct StateInfo* pState) 
     char* old_text_entry = 0;
     int old_text_len = LLCHAR_to_pchar(handle, State.head, &old_text_entry);
     
-    for (int i = 0; i < sizeof(TEST_STRING) - 1; i++) {
-        SendMessage(hwnd, WM_CHAR, TEST_STRING[i], 0);
+    for (int i = 0; i < sizeof(TEST_STRING_SPECIAL_KEY_TEXT_ENTRY) - 1; i++) {
+        SendMessage(hwnd, WM_CHAR, TEST_STRING_SPECIAL_KEY_TEXT_ENTRY[i], 0);
     }
     
     char* new_text_entry = 0;
     int new_text_len = LLCHAR_to_pchar(handle, State.head, &new_text_entry);
     
-    if (new_text_len - old_text_len == sizeof(REAL_TEST_STRING) - 1){
-        if (strncmp(REAL_TEST_STRING, new_text_entry + old_text_len, new_text_len - old_text_len) == 0) {
+    if (new_text_len - old_text_len == sizeof(TEST_STRING_REAL_SPECIAL_KEY_TEXT_ENTRY) - 1){
+        if (strncmp(TEST_STRING_REAL_SPECIAL_KEY_TEXT_ENTRY, new_text_entry + old_text_len, new_text_len - old_text_len) == 0) {
             pass = 0; //Don't want to set directly due to return value of strncmp
         }
     }
     
     return pass;
     
+}
+
+int testAlotOfEntry(HWND hwnd, HANDLE handle, struct StateInfo* pState) {
+    printf("[*] EVALUATING TEST AlotOfEntry [          ]\r");
+    struct StateInfo State = {0};
+    int CHAR_COUNT = 9999;
+    int TEST_CHAR = 'a';
+    
+    if (!ReadProcessMemory(handle, pState, &State, sizeof(State), 0)) return -1;
+    
+    char* old_text_entry = 0;
+    int old_text_len = LLCHAR_to_pchar(handle, State.head, &old_text_entry);
+    
+    int progress = 0;
+    for (int i = 0; i < CHAR_COUNT; i++) {
+        if ((i*10) / CHAR_COUNT > progress) {
+            progress += 1;
+            printf("[*] EVALUATING TEST AlotOfEntry [");
+            for (int j = 0; j < progress; j++) { printf("-"); }
+            for (int j = 0; j < 10-progress; j++) { printf(" "); }
+            printf("]\r");
+        }
+        SendMessage(hwnd, WM_CHAR, TEST_CHAR, 0);
+    }
+    printf("[*] EVALUATING TEST AlotOfEntry [----------]\r");
+    char* new_text_entry = 0;
+    int new_text_len = LLCHAR_to_pchar(handle, State.head, &new_text_entry);
+    
+    if (new_text_len - old_text_len != CHAR_COUNT) {
+        return new_text_len - old_text_len;
+    }
+    
+    return 0;
+}
+
+int testUndo(HWND hwnd, HANDLE handle, struct StateInfo* pState) {
+    struct StateInfo State = {0};
+    
+    if (!ReadProcessMemory(handle, pState, &State, sizeof(State), 0)) return -1;
+    
+    char* old_text_entry = 0;
+    int old_text_len = LLCHAR_to_pchar(handle, State.head, &old_text_entry);
+    
+    testBasicTextEntry(hwnd, handle, pState);
+    testSpecialKeyTextEntry(hwnd, handle, pState);
+    
+    int undoAmt = sizeof(TEST_STRING_SPECIAL_KEY_TEXT_ENTRY) + sizeof(TEST_STRING_BASIC_TEXT_ENTRY) - 2;
+    
+    for (int i = 0; i < undoAmt; i++) {
+        SendMessage(hwnd, WM_COMMAND, 'Z', 0);
+    }
+    
+    char* new_text_entry = 0;
+    int new_text_len = LLCHAR_to_pchar(handle, State.head, &new_text_entry);
+    
+    if (strncmp(old_text_entry, new_text_entry, old_text_len) == 0) {
+        return 0;
+    }
+    return old_text_len - new_text_len;
 }
 
 
@@ -136,7 +195,10 @@ int main() {
     if ((test = testMakeNewDocument(hwnd, handle, pState))) printf("[-] FAILED TEST MakeNewDocument! %d\n", test); else printf("[+] PASSED TEST MakeNewDocument!\n");
     if ((test = testBasicTextEntry(hwnd, handle, pState))) printf("[-] FAILED TEST BasicTextEntry! %d\n", test); else printf("[+] PASSED TEST BasicTextEntry!\n");
     if ((test = testSpecialKeyTextEntry(hwnd, handle, pState))) printf("[-] FAILED TEST SpecialKeyTextEntry! %d\n", test); else printf("[+] PASSED TEST SpecialKeyTextEntry!\n");
-    
+    if ((test = testAlotOfEntry(hwnd, handle, pState))) printf("[-] FAILED TEST AlotOfEntry! %d                      \n", test); else printf("[+] PASSED TEST AlotOfEntry!                      \n");
+    if ((test = testUndo(hwnd, handle, pState))) printf("[-] FAILED TEST Undo! %d                      \n", test); else printf("[+] PASSED TEST Undo!                      \n");
+    //if ((test = testMakeNewDocument(hwnd, handle, pState))) printf("[-] FAILED TEST MakeNewDocument2! %d\n", test); else printf("[+] PASSED TEST MakeNewDocument2!\n");
+
     LABEL_END:
     if (handle) {
         CloseHandle(handle);
